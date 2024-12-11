e55c1587396e5cf9068a5a3065ede5d22cc88f8e
sora_uzu <naruto07170717@gmail.com>
2024-12-11 15:59:05 +0900

    add random function

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -12,37 +12,18 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
-    
-    var leftDiceNumber = 1
-    var rightDiceNumber = 5
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-    }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         
-        diceImageView1.image = [
-            UIImage(imageLiteralResourceName: "DiceOne"),
-            UIImage(imageLiteralResourceName: "DiceTwo"),
-            UIImage(imageLiteralResourceName: "DiceThree"),
-            UIImage(imageLiteralResourceName: "DiceFour"),
-            UIImage(imageLiteralResourceName: "DiceFive"),
-            UIImage(imageLiteralResourceName: "DiceSix")
-        ][leftDiceNumber]
+        let diceArray = [UIImage(imageLiteralResourceName: "DiceOne"),
+                         UIImage(imageLiteralResourceName: "DiceTwo"),
+                         UIImage(imageLiteralResourceName: "DiceThree"),
+                         UIImage(imageLiteralResourceName: "DiceFour"),
+                         UIImage(imageLiteralResourceName: "DiceFive"),
+                         UIImage(imageLiteralResourceName: "DiceSix")]
         
-        diceImageView2.image = [
-            UIImage(imageLiteralResourceName: "DiceOne"),
-            UIImage(imageLiteralResourceName: "DiceTwo"),
-            UIImage(imageLiteralResourceName: "DiceThree"),
-            UIImage(imageLiteralResourceName: "DiceFour"),
-            UIImage(imageLiteralResourceName: "DiceFive"),
-            UIImage(imageLiteralResourceName: "DiceSix")
-        ][rightDiceNumber]
-        
-        leftDiceNumber = leftDiceNumber + 1
-        rightDiceNumber = rightDiceNumber - 1
+        diceImageView1.image = diceArray.randomElement()
+        diceImageView2.image = diceArray.randomElement()
     }
     
 }
